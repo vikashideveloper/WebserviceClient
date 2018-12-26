a7b3401251e6640a2f63ba48e55c6307f8924161
Vikash Kumar <vikash.ideveloper@gmail.com>
2018-12-26 13:12:20 +0530

    Initial Commit

---
+++ WebserviceClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebserviceClient
+//
+//  Created by Vikash on 26/12/18.
+//  Copyright © 2018 Vikash Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
